de9cb4d9139e4af2f2dc3aa31e3ba4512884aac2
Byevich Dmytro <71142218+sunnypiase@users.noreply.github.com>
2023-07-18 22:52:35 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,6 +7,17 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins",
+        builder =>
+        {
+            builder.AllowAnyOrigin()
+                   .AllowAnyMethod()
+                   .AllowAnyHeader();
+        });
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -48,6 +59,8 @@ else
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowAllOrigins"); // Apply the CORS policy
 
 app.UseAuthorization();
 
