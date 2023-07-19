6c9655c5da51d8b0e69c58ed472da3ed590463e4
Byevich Dmytro <71142218+sunnypiase@users.noreply.github.com>
2023-07-19 10:20:26 +0300

    allow cors for frontend

--- Program.cs
+++ Program.cs
@@ -5,22 +5,25 @@ using WebAPI.Repositories;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("AllowAllOrigins",
+    options.AddPolicy("AllowSpecificOrigins",
         builder =>
         {
-            builder.AllowAnyOrigin()
+            builder.WithOrigins(
+                   "http://localhost:3000",
+                   "https://64b776f28321af4be0486692--pegas.netlify.app/")
                    .AllowAnyMethod()
-                   .AllowAnyHeader();
+                   .AllowAnyHeader()
+                   .AllowCredentials();
         });
 });
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 if (builder.Environment.IsDevelopment())
 {
     builder.Services.AddScoped(typeof(IRepository<>), typeof(SqlServerRepository<>));
@@ -39,12 +42,10 @@ else
 {
     builder.Services.AddDbContext<NpgsqlHotelDbContext>(options =>
         options.UseNpgsql(ConnectionHelper.GetConnectionString(builder.Configuration)));
-
 }
 
 
 var app = builder.Build();
-
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -60,7 +61,7 @@ else
 
 app.UseHttpsRedirection();
 
-app.UseCors("AllowAllOrigins"); // Apply the CORS policy
+app.UseCors("AllowSpecificOrigins"); // Apply the CORS policy
 
 app.UseAuthorization();
 
