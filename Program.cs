5cccdae17d6675e1c2e0062a6a4bc0eb621e8ede
Byevich Dmytro <71142218+sunnypiase@users.noreply.github.com>
2023-07-19 08:42:46 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,23 +7,20 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-// Add CORS services and define a policy
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("AllowSpecificOrigin",
-        policyBuilder =>
+    options.AddPolicy("AllowAllOrigins",
+        builder =>
         {
-            policyBuilder.WithOrigins("http://localhost:3000")
-                         .AllowAnyMethod()
-                         .AllowAnyHeader()
-                         .AllowCredentials();
+            builder.AllowAnyOrigin()
+                   .AllowAnyMethod()
+                   .AllowAnyHeader();
         });
 });
 
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 if (builder.Environment.IsDevelopment())
 {
     builder.Services.AddScoped(typeof(IRepository<>), typeof(SqlServerRepository<>));
@@ -63,7 +60,7 @@ else
 
 app.UseHttpsRedirection();
 
-app.UseCors("AllowSpecificOrigin"); // Use the CORS policy
+app.UseCors("AllowAllOrigins"); // Apply the CORS policy
 
 app.UseAuthorization();
 
